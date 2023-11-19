9d66c577dfbe3b3ee75a1ceeb450a56355b0d984
MarcoVND <up230432@alumnos.upa.edu.mx>
2023-11-18 19:26:46 -0600

    second commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div data-theme="rick_morty" class="container h-screen bg-base-100 flex flex-col items-center">
+  <div data-theme="rick_morty" class="w-screen h-screen bg-base-100 flex flex-col items-center">
     <div class="w-4/5 pt-5">
       <input type="text" class="input input-primary w-full" placeholder="Buscar" />
     </div>
